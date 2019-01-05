90bc06796705366e7973cd9a46ada407546b9bec
Weverton Souza <wevertonad@gmail.com>
2019-01-05 18:37:45 -0200

    Adding breadCrumbComponent

---
+++ src/app/shared/components/bread-crumb/bread-crumb.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { link } from 'fs';
+
+interface BreadCrumbItem {
+  text: string,
+  link?: string
+}
+
+@Component({
+  selector: 'app-bread-crumb',
+  templateUrl: './bread-crumb.component.html',
+  styleUrls: ['./bread-crumb.component.css']
+})
+export class BreadCrumbComponent implements OnInit {
+
+  @Input() items: Array<BreadCrumbItem> = [];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  isLastItem(item: BreadCrumbItem): boolean {
+    const index = this.items.indexOf(item);
+    return index + 1 == this.items.length;
+  }
+
+}
